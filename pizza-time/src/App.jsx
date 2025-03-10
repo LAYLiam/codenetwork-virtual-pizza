a7566e1b6135314a466c1dd0e931824bc92ed757
Liam Lay <144472669+LAYLiam@users.noreply.github.com>
2025-03-10 16:36:08 +1000

    Pizza working and popup working

--- pizza-time/src/App.jsx
+++ pizza-time/src/App.jsx
@@ -4,6 +4,7 @@ import Pizzas from "./routes/pizzaboxes";
 import Other from "./routes/other";
 import Receipt from "./routes/receipt";
 import Leaderboard from "./routes/leaderboard";
+import Consume from "./routes/consume"
 
 export default function App() {
   return (
@@ -14,7 +15,8 @@ export default function App() {
           <Route path="/pizza" element={<Pizzas />} />
           <Route path="/receipt" element={<Receipt/>} />
           <Route path="/leaderboard" element={<Leaderboard/>} />
-          <Route path="/other" element={<Other />} />
+          <Route path="/other" element={<Other/>} />
+          <Route path="/consume" element={<Consume/>} />
         </Routes>
       </BrowserRouter>
     </div>
